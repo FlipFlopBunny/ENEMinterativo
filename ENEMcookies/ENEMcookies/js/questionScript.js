1bef2d16922c5140f3a6bc37c12780a7a3fdc236
Giovana Carla Pereira Silva <giovanacarla02@gmail.com>
2022-05-29 20:07:19 -0300

    ENEM Cookies
    First time on GitHub, I don't know what I'm doing

---
+++ ENEMcookies/ENEMcookies/js/questionScript.js
@@ -0,0 +1,30 @@
+﻿var score = 0;
+var check = 0;
+
+//function start () {
+//	check = 0;
+//	score = 0;
+//}
+
+function correctFunction(idButton) {
+
+    if (idButton == "correct") {
+        document.getElementById("answer").innerHTML = "correta"
+        if (check == 0) {
+            score++
+            check++
+        }
+        document.getElementById("score").innerHTML = "score: " + score;
+        document.getElementById("correct").style.backgroundColor = "#c6faac";
+        document.getElementById("correct").style.borderColor = "#50eb02";
+    }
+    else {
+        document.getElementById("answer").innerHTML = "incorreta"
+        check++
+        document.getElementById("score").innerHTML = "score: " + score;
+    }
+}
+
+function proximaFunction() {
+    check = 0;
+}
